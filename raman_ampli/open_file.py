136778a39f68185f322bacc5876411451b9ef5a2
patquem <patrick.quemere@cea.fr>
2025-09-10 14:48:53 +0200

    bugfix: return file_path if not None

--- raman_ampli/open_file.py
+++ raman_ampli/open_file.py
@@ -2,14 +2,15 @@ import tkinter as tk
 from tkinter import filedialog as fd
 import os
 
+
 # In[choose file]
 
 def open_file_selection():
     file = fd.askopenfile()
-    #print(label)
-    #print(file.read())
+    # print(label)
+    # print(file.read())
     if file:
-      file_path = os.path.abspath(file.name)
-    return file_path
+        file_path = os.path.abspath(file.name)
+        return file_path
 
-#file_path = open_file_selection()
+# file_path = open_file_selection()
